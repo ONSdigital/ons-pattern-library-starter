e5ce99a93ab1c2c8d984185aa892156cc11fdd09
Crispioso <crispin.merriman@gmail.com>
2016-03-31 16:22:09 +0100

    Update ship-grey classes

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,19 +11,19 @@ Show and hide grid overlay on examples
 $(function() {
     $('.js-show-grid').click(function() {
         var $this = $(this),
-            $example = $this.siblings('.pl-example__rendered');
+            $example = $this.siblings('.pl-wrapper');
 
         function gridVisible() {
-            if ($example.hasClass('pl-example__rendered--grid')) {
+            if ($example.hasClass('pl-wrapper--grid')) {
                 return true;
             }
         }
 
         if (gridVisible()) {
-            $example.removeClass('pl-example__rendered--grid')
+            $example.removeClass('pl-wrapper--grid')
             $this.text('Show grid');
         } else {
-            $example.addClass('pl-example__rendered--grid');
+            $example.addClass('pl-wrapper--grid');
             $this.text('Hide grid');
         }
 
